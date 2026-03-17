3bc0f636a58090215b8244653ca037834f986d0a
Jhon <you@example.comC:/Users/Jhon/AppData/Local/Programs/Python/Python312/python.exe>
2026-03-17 14:27:34 -0500

    agrego a juego 1

---
+++ juego1/juego_1/logica_juego1.py
@@ -0,0 +1,33 @@
+# LIBRERIAS
+
+import os
+import random
+
+
+# se define una funcion que toma un png de manos aelatorio que recibe:
+
+def choise_image()->str:
+
+    # se crea una tupla que guarda todas las carpetas
+    subcarpetas = [f for f in os.listdir("juego1/asl_dataset") if os.path.isdir(os.path.join("juego1/asl_dataset", f))]
+
+    # se elige una sola carpeta al azar
+    carpeta_elegida = random.choice(subcarpetas)
+    # se construye la ruta de la carpeta elegida
+    ruta_carpeta_elegida = os.path.join("juego1/asl_dataset", carpeta_elegida)
+
+    # se listan todos los elementos que estan dentro de la carpeta elegida
+    imagenes = os.listdir(ruta_carpeta_elegida)
+
+    # al final del proceso se genera una ruta de una de las imagenes
+    imagen_seleccionada = random.choice(imagenes)
+    ruta_imagen_seleccionada = os.path.join(ruta_carpeta_elegida, imagen_seleccionada)
+    
+    return ruta_imagen_seleccionada
+
+# guarda la ruta de la imagen en una variable
+ruta_imagen_escojida = choise_image()
+
+
+
+
